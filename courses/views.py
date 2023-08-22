c2414b8b105904866c41517ab693e6d60db5ef13
Michael Pacleb <michael.pacleb@gmail.com>
2023-08-21 20:05:30 -0400

    Created public views for displaying course information. Available courses are listed and optionally filtered by subject. Also created a template to display a single course overview.

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.base import TemplateResponseMixin, View
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
@@ -7,8 +8,10 @@ from django.forms.models import modelform_factory
 from django.apps import apps
 from django.urls import reverse_lazy
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+from django.db.models import Count
 
-from .models import Course, Module, Content
+
+from .models import Subject, Course, Module, Content
 from .forms import ModuleFormSet
 
 
@@ -163,3 +166,29 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
             )
 
         return self.render_json_response({"saved": "Ok"})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = "courses/course/list.html"
+
+    def get(self, req, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count("courses"))
+        courses = Course.objects.annotate(total_modules=Count("modules"))
+
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+
+        return self.render_to_response(
+            {
+                "subjects": subjects,
+                "subject": subject,
+                "courses": courses,
+            }
+        )
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = "courses/course/detail.html"
